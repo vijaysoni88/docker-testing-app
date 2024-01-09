b65892242513781ba4a7df0b351ddc64b40c219d
vijaysoni7 <vijay.soni011@gmail>
2024-01-09 12:47:21 +0530

    generate pdf

--- app/controllers/admin/job_sheets_controller.rb
+++ app/controllers/admin/job_sheets_controller.rb
@@ -1,35 +1,30 @@
 class Admin::JobSheetsController < ApplicationController
   def create
-    response = DirectionsApiService.get_directions(params[:start_address], params[:end_address])
+    # response = DirectionsApiService.get_directions(params[:start_address], params[:end_address])
   
-    if response.success?
-      directions = {
-        steps: response['steps'],
-        instructions: response['instructions']
-      }
+    # if response.success?
+    #   directions = {
+    #     steps: response['steps'],
+    #     instructions: response['instructions']
+    #   }
 
-      @job_sheet = JobSheet.new(job_sheet_params.merge(directions: directions))
-
+      # @job_sheet = JobSheet.new(job_sheet_params.merge(directions: directions))
+      @job_sheet = JobSheet.new(job_sheet_params)
       if @job_sheet.save
-        PdfGeneratorService.generate_job_sheet_pdf(@job_sheet)
-        respond_to do |format|
-          format.js { render 'create', status: :ok }
-        end
+        # binding.pry
+        pdf_data = PdfGeneratorService.generate_job_sheet_pdf(@job_sheet)
+
+          send_data pdf_data.render, filename:'job_sheet.pdf', type: "application/pdf", disposition: :attachment
       else
         respond_to do |format|
           format.js { render 'error', status: :unprocessable_entity }
         end
       end
-    else
-      respond_to do |format|
-        format.js { render 'error', status: :unprocessable_entity }
-      end
-    end
   end
-    
+
   private
     
   def job_sheet_params
-    params.require(:job_sheet).permit(:start_address, :end_address, :customer_name, :site_contact, :contact_number, :notes)
+    params.require(:job_sheet).permit(:start_address, :end_address, :customer_name, :site_contact, :contact_number, :notes, :job_number, :job_instruction)
   end
 end
---
+++ app/services/directions_api_service.rb
@@ -0,0 +1,6 @@
+class DirectionsApiService
+  def self.get_directions(start_address, end_address)
+    # Implement the logic to make a request to the Directions API and parse the response
+    # Return a hash with 'steps' and 'instructions'
+  end
+end
